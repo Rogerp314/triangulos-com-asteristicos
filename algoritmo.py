80828d84279d301602e56ad746c53630e344bbe3
Pedro Rogério <138505724+Rogerp314@users.noreply.github.com>
2024-08-30 13:04:20 -0300

    Atualização

--- algoritmo.py
+++ algoritmo.py
@@ -1,4 +1,13 @@
-base = int(input('Quantos asterísticos terá a base de seu triângulo retângulo? '))
-caracteres = '*'
-for i in range(1, base+1):
-    print(caracteres * i)
+v = True
+while v == True:
+    print('-='*20)
+    base = int(input('Quantos asterísticos terá a base de seu triângulo retângulo? '))
+    caracteres = '*'
+    for i in range(1, base+1):
+        print(caracteres * i)
+    pergunta = input('Deseja continuar? [S/N]: ').strip().lower()[0]
+    if pergunta == 's':
+        v = True
+    else:
+        v = False
+print('-=-=-=-= FIM DO PROGRAMA -=-=-=-=')
